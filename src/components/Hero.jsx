d04854260ce5e08706e384b73bbf77be650785f0
Igbagbo <iolufiranye@gmail.com>
2024-04-18 20:25:10 +0100

    yesssgit status!

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,8 +18,8 @@ function Hero() {
         <section className=' text-white hero mt-20 flex w-[80vw] pl-[3vw] overflow-hidden'>
             <motion.div variants={textVariant('.2')} className='hero-left w-[459px] h-[461px] mt-5 ml-[-4vw]'>
 
-                <h1 className='hero-head text-[80px] w-[12.2cm] h-[231px] leading-[77px] pl-[2.5vw] font-bold'>Grow your subscription business</h1>
-                <p className='hero-p pl-[2.5vw] text-[#bebcbc] text-[20px] leading-[30px] tracking-[-0.63px] mt-10'>Outcome centered products that reduce churn, optimize pricing, and grow subscription business end-to-end.</p>
+                <h1 className='hero-head text-[80px] w-[12.2cm] h-[231px] leading-[77px] pl-[2.5vw] font-bold'>Learn to Code</h1>
+                <p className='hero-p pl-[2.5vw] text-[#bebcbc] text-[20px] leading-[30px] tracking-[-0.63px] mt-10'>Learn programming through weekly newsletters explaining the concept thoroughly.</p>
 
                 <motion.div variants={slideIn('left', 'tween', '.5s', '.3s')} className='start pl-[2.5vw] mt-10 flex hero-btns'>
                 <motion.button 
